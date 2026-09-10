d2eeb4251a81019594d1a002d345ae718c977723
Krish318171 <krishnasamantabadra3588@gmail.com>
2026-09-10 14:29:11 +0700

    update code

--- Main.java
+++ Main.java
@@ -1,13 +1,119 @@
-abstract class Perangkat{
+interface connectable{
+    void connect();
+    void disconnect();
+}
+
+interface switchable{
+    
+}
+
+interface lockable{
+    void lock();
+    void unlock();
+}
+
+
+
+abstract class smartDevice{
     private int id;
     private String nama;
     private int daya;
     private int volume;
 
-    Perangkat(int id, String nama, int daya, int volume){
+    smartDevice(int id, String nama, int daya, int volume){
         this.id = id;
         this.nama = nama;
         this.daya = daya;
         this. volume = volume;
+        
+    }
+
+    abstract  void turnOn();
+    abstract  void turnOf();
+    abstract  void Status();
+    String printData(){
+        return "id : " + id
+        + "\nnama : " + nama
+        + "\ndaya : " + daya
+        + "\nvolume : " + volume;
+
+    }
+
+}
+
+class smartTV extends smartDevice implements connectable{
+
+    smartTV(int id, String nama, int daya, int volume){
+        super(id, nama, daya, volume);
+
+    }
+
+    @Override 
+    public void turnOn(){
+        System.out.println("On");
+    }
+
+    @Override 
+    public void turnOf("Off"){
+
+    }
+
+    @Override 
+    public void connect(){
+        System.out.println("Connect");
+    }
+
+    @Override 
+    public  void disconnect(){
+        System.out.println("Disconnect");
+    }
+    
+}
+
+
+class smartSpeaker extends smartDevice implements connectable{
+
+    smartSpeaker(int id, String nama, int daya, int volume){
+        super(id, nama, daya, volume);
+    }
+
+    @Override 
+    public void turnOn(){
+        System.out.println("On");
+    }
+
+    @Override 
+    public void turnOf(){
+        System.out.println("Off");
+
+    }
+
+    @Override 
+    public void connect(){
+        System.out.println("Connect");
+    }
+
+    @Override 
+    public  void disconnect(){
+        System.out.println("Disconnect");
+    }
+    
+}
+
+
+class smartDoorLock extends smartDevice implements lockable{
+
+    smartDoorLock(int id, String nama, int daya){
+        super(id, nama, daya);
+    }
+    
+    @Override 
+    public void lock(){
+        System.out.println("Lock");
+    }
+
+    @Override 
+    public void unlock(){
+        System.out.println("Unlock");
     }
 }
