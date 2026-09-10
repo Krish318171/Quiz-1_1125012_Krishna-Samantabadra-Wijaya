9e365797088292404e3b2fece49fa2c432530a19
Krish318171 <krishnasamantabadra3588@gmail.com>
2026-09-10 13:17:07 +0700

    initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+abstract class Perangkat{
+    private int id;
+    private String nama;
+    private int daya;
+    private int volume;
+
+    Perangkat(int id, String nama, int daya, int volume){
+        this.id = id;
+        this.nama = nama;
+        this.daya = daya;
+        this. volume = volume;
+    }
+}
